6a504502221174df899b2465f003f41fed8f9381
Laerte <lhzaccarelli@gmail.com>
2021-05-27 10:01:42 -0300

    Final server - Pure Node

--- index.js
+++ index.js
@@ -5,25 +5,56 @@ const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    
+    // Extension of file
+    let extname = path.extname(filePath)
 
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content)
-        })
+    // Initial content type
+    let contentType = 'text/html'
 
+    // Check ext and set content type
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.json':
+            contentType = 'application/json'
+            break
+        case '.png':
+            contentType = 'image/png'
+            break
+        case '.jpg':
+            contentType = 'image/jpg'
+            break
+        case '.ico':
+            contentType = 'image/vnd.microsoft.icon'
+            break
     }
 
-    // API Example
-    if (req.url === "/api/users") {
-        const users = [
-            { name: 'John Doe', age: 40 },
-            { name: 'Bob Smith', age: 20 },
-        ]
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(users))
-    }
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.end(content, 'utf8')
+                })
+            } else {
+                // Some server error
+                res.writeHead(500)
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType })
+            res.end(content, 'utf8')
+        }
+    })
 })
 
 
